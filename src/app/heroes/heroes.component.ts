026fd622cb877c3f084f6d61ff3d632303ba2b0e
vanande <101594646+vanande@users.noreply.github.com>
2023-05-05 07:24:28 +0100

    Add hero services and message

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from "../mock-heroes";
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +8,16 @@ import {HEROES} from "../mock-heroes";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) { }
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
