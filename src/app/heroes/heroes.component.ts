efd6651cfe59188e4316c6433810914cddc7ec4f
vanande <101594646+vanande@users.noreply.github.com>
2023-05-05 17:07:12 +0100

    Shared hero services with other component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,13 @@
-import {HeroService} from "../hero.service";
-import {Hero} from "../hero";
-import {OnInit} from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
 export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
